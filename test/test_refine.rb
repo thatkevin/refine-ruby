f848aa58c94e6c35b3cbfe07f8d73effca40a2ad
Allie Dukuze <Alliedukuze@gmail.com>
2018-04-24 17:01:40 -0700

    Refactors to spec api diff --git a/test/test_refine.rb b/test/test_refine.rb index 454ec9b..543582d 100644 --- a/test/test_refine.rb +++ b/test/test_refine.rb @@ -2,34 +2,35 @@ gem 'minitest'  require 'minitest/autorun'  require_relative '../lib/refine.rb'
    -class TestRefine < MiniTest::Unit::TestCase
    
    -  def setup
    +describe Refine do
    +
    +  before do
         @refine_project = Refine.new({ "project_name" => 'date_cleanup', "file_name" => './test/dates.txt' })
       end
    -
    -	def test_refine_initializer_has_instance_variable_project_name
    +
    +	it "refine_initializer_has_instance_variable_project_name" do
     		assert_equal 'date_cleanup', @refine_project.project_name
     	end
    -
    -  def test_refine_initializer_has_instance_variable_project_id
    +
    +  it "refine_initializer_has_instance_variable_project_id" do
         assert @refine_project.project_id.match(/^[0-9]+$/)
       end
    -
    -  def test_get_all_project_metadata
    +
    +  it "get_all_project_metadata" do
         assert Refine.get_all_project_metadata.instance_of? Hash
       end
    -
    -  def test_apply_operations
    +
    +  it "apply_operations" do
         assert @refine_project.apply_operations( './test/operations.json' )
       end
    -
    -  def test_call
    +
    +  it "call" do
          assert @refine_project.call( 'apply-operations', 'operations' => File.read( './test/operations.json' ) )
       end
    -
    -  def after_tests
    +
    +  after do
         @refine_project.delete_project
       end
    -
    +
     end

--- test/test_refine.rb
+++ test/test_refine.rb
@@ -2,34 +2,35 @@ gem 'minitest'
 require 'minitest/autorun'
 require_relative '../lib/refine.rb'
 
-class TestRefine < MiniTest::Unit::TestCase
 
-  def setup
+describe Refine do
+
+  before do
     @refine_project = Refine.new({ "project_name" => 'date_cleanup', "file_name" => './test/dates.txt' })
   end
-	
-	def test_refine_initializer_has_instance_variable_project_name
+
+	it "refine_initializer_has_instance_variable_project_name" do
 		assert_equal 'date_cleanup', @refine_project.project_name
 	end
-  
-  def test_refine_initializer_has_instance_variable_project_id
+
+  it "refine_initializer_has_instance_variable_project_id" do
     assert @refine_project.project_id.match(/^[0-9]+$/)
   end
-  
-  def test_get_all_project_metadata
+
+  it "get_all_project_metadata" do
     assert Refine.get_all_project_metadata.instance_of? Hash
   end
-  
-  def test_apply_operations
+
+  it "apply_operations" do
     assert @refine_project.apply_operations( './test/operations.json' )
   end
-  
-  def test_call
+
+  it "call" do
      assert @refine_project.call( 'apply-operations', 'operations' => File.read( './test/operations.json' ) )
   end
-    
-  def after_tests
+
+  after do
     @refine_project.delete_project
   end
-  
+
 end
